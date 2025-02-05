3085d50a4dac2635d135cfe8a34f8f93e351a2db
Sandra <oyarekhuas@gmail.com>
2025-02-05 02:08:07 +0100

    Refactor Signin component layout

--- ventify/src/components/Header.tsx
+++ ventify/src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <img src="/logo.png" alt="logo" />
 
         <div className="hidden md:flex items-center space-x-8">

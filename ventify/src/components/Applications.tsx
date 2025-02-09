e517372a6e3c52ed6a7ed43babc43214e14951ab
Sandra <oyarekhuas@gmail.com>
2025-02-09 20:15:02 +0100

    Completed Applications component

--- ventify/src/components/Applications.tsx
+++ ventify/src/components/Applications.tsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { BiTrash } from "react-icons/bi";
 
 const Applications = () => {
-  const [activeMainSection, setActiveMainSection] =
-    useState<string>("Applications");
-  const [activeSubsection, setActiveSubsection] = useState<string>(
+  const [activeMainSection, setActiveMainSection] = useState<string>(
     "Portfolio Companies"
   );
+  const [activeSubsection, setActiveSubsection] = useState<string>("Admitted");
   const [companies, setCompanies] = useState([
     { id: 1, name: "Company A" },
     { id: 2, name: "Company B" },
@@ -14,15 +13,22 @@ const Applications = () => {
     { id: 4, name: "Company D" },
     { id: 5, name: "Company E" },
   ]);
+  const raisedAmt = [
+    { id: 1, amount: "$400" },
+    { id: 2, amount: "$200" },
+    { id: 3, amount: "$100" },
+    { id: 4, amount: "$500" },
+    { id: 5, amount: "$800" },
+  ];
 
   const handleDelete = (id: number) => {
     setCompanies(companies.filter((company) => company.id !== id));
   };
 
-  const subsections: Record<string, JSX.Element> = {
+  const PCsubsections: Record<string, JSX.Element> = {
     Admitted: (
       <div
-        className="w-full border-[#A09E9E] border-[1px] py-2 px-6 mt-5 rounded-md
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-6 mt-5 rounded-md
         flex flex-col flex-grow min-h-[60vh]"
       >
         {companies.map((company) => (
@@ -44,36 +50,204 @@ const Applications = () => {
       </div>
     ),
     Pending: (
-      <div>
-        <p>Tab 2 Content</p>
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-12 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <button className="mr-4 border-[0.5px] rounded-md py-1 bg-red-400 px-20 hover:cursor-pointer hover:bg-red-200">
+              Pending
+            </button>
+            <button className="mr-4 border-[0.5px] rounded-md py-1 bg-green-400 px-20 hover:cursor-pointer hover:bg-green-200">
+              Decline
+            </button>
+          </div>
+        ))}
       </div>
     ),
     Declined: (
-      <div>
-        <p>Tab 3 Content</p>
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-20 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p>01/24/2025</p>
+          </div>
+        ))}
       </div>
     ),
     "View All": (
-      <div>
-        <p>Tab 4 Content</p>
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 justify-between items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p className="border-[0.5px] px-10 py-1 rounded-md">Declined</p>
+            <p>01/24/2025</p>
+            <p>12ADG2020</p>
+          </div>
+        ))}
       </div>
     ),
     Deleted: (
-      <div>
-        <p>Tab 5 Content</p>
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-20 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p>01/24/2025</p>
+          </div>
+        ))}
       </div>
     ),
     Raised: (
-      <div>
-        <p>Tab 6 Content</p>
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] text-center border-[1px] pb-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        <div className="flex py-4 gap-4 justify-between mb-3 items-center font-bold border-b-[1px] border-gray-400">
+          <p className="w-1/4">Company Name</p>
+          <p className="w-1/4">Stage</p>
+          <p className="w-1/4">Date</p>
+          <p className="w-1/4">Amount</p>
+        </div>
+        {companies.map((company, index) => (
+          <div
+            key={company.id}
+            className="flex gap-4 py-2 justify-between items-center mb-3"
+          >
+            <p className="w-1/4">{company.name}</p>
+            <p className="border-[0.5px] w-1/4 py-1 rounded-md">Declined</p>
+            <p className="w-1/4">01/24/2025</p>
+            <p className="w-1/4">{raisedAmt[index]?.amount || "N/A"}</p>
+          </div>
+        ))}
       </div>
     ),
   };
+
+  const INsubsections: Record<string, JSX.Element> = {
+    Admitted: (
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-6 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 justify-between items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <button className="mr-4 border-[0.5px] rounded-md px-18">
+              Request for more documents
+            </button>
+            <BiTrash
+              size={24}
+              className="cursor-pointer text-black hover:text-gray-300"
+              onClick={() => handleDelete(company.id)}
+            />
+          </div>
+        ))}
+      </div>
+    ),
+    Pending: (
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-12 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <button className="mr-4 border-[0.5px] rounded-md py-1 bg-red-400 px-20 hover:cursor-pointer hover:bg-red-200">
+              Pending
+            </button>
+            <button className="mr-4 border-[0.5px] rounded-md py-1 bg-green-400 px-20 hover:cursor-pointer hover:bg-green-200">
+              Decline
+            </button>
+          </div>
+        ))}
+      </div>
+    ),
+    Declined: (
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-20 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p>01/24/2025</p>
+          </div>
+        ))}
+      </div>
+    ),
+    "View All": (
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 justify-between items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p className="border-[0.5px] px-10 py-1 rounded-md">Declined</p>
+            <p>01/24/2025</p>
+            <p>12ADG2020</p>
+          </div>
+        ))}
+      </div>
+    ),
+    Deleted: (
+      <div
+        className="w-[90%] mx-auto border-[#A09E9E] border-[1px] py-5 px-12 mt-5 rounded-md
+        flex flex-col flex-grow min-h-[60vh]"
+      >
+        {companies.map((company) => (
+          <div
+            key={company.id}
+            className="flex px-3 py-2 gap-20 items-center mb-3"
+          >
+            <p>{company.name}</p>
+            <p>01/24/2025</p>
+          </div>
+        ))}
+      </div>
+    ),
+  };
+
   const sections: Record<string, JSX.Element> = {
     "Portfolio Companies": (
       <div>
         <nav className=" w-full flex gap-5 items-center justify-between py-2 px-4">
-          {Object.keys(subsections).map((section) => (
+          {Object.keys(PCsubsections).map((section) => (
             <button
               key={section}
               className={`px-1 py-1 w-full text-center border-[1.5px] rounded-md ${
@@ -88,12 +262,28 @@ const Applications = () => {
           ))}
         </nav>
 
-        <div>{subsections[activeSubsection]}</div>
+        <div>{PCsubsections[activeSubsection]}</div>
       </div>
     ),
     Investors: (
       <div>
-        <p>Cheeee!!!</p>
+        <nav className=" w-full flex gap-5 items-center justify-between py-2 px-4">
+          {Object.keys(INsubsections).map((section) => (
+            <button
+              key={section}
+              className={`px-1 py-1 w-full text-center border-[1.5px] rounded-md ${
+                activeSubsection === section
+                  ? "border-none outline-none text-black"
+                  : ""
+              }`}
+              onClick={() => setActiveSubsection(section)}
+            >
+              {section}
+            </button>
+          ))}
+        </nav>
+
+        <div>{INsubsections[activeSubsection]}</div>
       </div>
     ),
   };
